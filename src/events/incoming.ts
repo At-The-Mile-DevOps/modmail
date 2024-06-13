efa58385c5cb313d251217746e6d2f4a4b25ac54
Tyler <88517473+tm21cy@users.noreply.github.com>
2024-06-13 03:09:06 -0400

    feat: add hidden messages to staff transcripts

--- src/events/incoming.ts
+++ src/events/incoming.ts
@@ -9,18 +9,28 @@ module.exports = {
 	name: Events.MessageCreate,
 	once: false,
 	async execute(message: Message) {
-		if (message.channel.type != ChannelType.DM) return
 		if (message.author.bot) return
-		const permit = await ModMailPrisma.GET.getUserPermit(message.author.id)
-		if (permit < Permit.EARLY_ACCESS_STAFF) return await message.reply({ content: "You currently do not have permission to access this ModMail feature." })
-		const channel = await ModMailPrisma.GET.getUserTicketObject(message.author.id)
-		if (!channel || !channel.channel) {
-			
-			await ticketOpenFlow(message)
-		} else {
-			await ticketReplyFlow(message, channel)
-		}
+		switch (message.channel.type) {
+			case ChannelType.DM: {
+				const permit = await ModMailPrisma.GET.getUserPermit(message.author.id)
+				if (permit < Permit.EARLY_ACCESS_STAFF) return await message.reply({ content: "You currently do not have permission to access this ModMail feature." })
+				const channel = await ModMailPrisma.GET.getUserTicketObject(message.author.id)
+				if (!channel || !channel.channel) {
 
-		return LogEmitter.emit('userMessage', message.author.id)
+					await ticketOpenFlow(message)
+				} else {
+					await ticketReplyFlow(message, channel)
+				}
+
+				return LogEmitter.emit('userMessage', message.author.id)
+			}
+			case ChannelType.GuildText: {
+				const status = await ModMailPrisma.GET.getChannelTicketUser(message.channel.id)
+				if (!status) return
+				else {
+					return await ModMailPrisma.POST.createNewSequencedMessage(status, message.author.id, message.url, message.content, '0', message.id, false, message.author.displayName, true, true)
+				}
+			}
+		}
 	}
 }
--- src/handlers/ticketClose.ts
+++ src/handlers/ticketClose.ts
@@ -11,12 +11,18 @@ export default async function ticketCloseFlow(message: Message) {
 		return [false, null, null]
 	} else {
 		const user = await ModMailPrisma.GET.getChannelTicketUser(channelId) as string // legal as we know the channel is bound atp
-		const transcriptText = await ModMailPrisma.GET.printTranscript(user)
+		const [transcriptText, staffTranscriptText] = await ModMailPrisma.GET.printTranscript(user)
 		transcriptText.push("System: Ticket closed.")
 		const buffer = Buffer.from(transcriptText.join(`\n`), "utf-8")
 		const transcriptAttachment = new AttachmentBuilder(buffer, {
 			name: "transcript.txt",
 			description: "A transcript of your ModMail conversation."
+		})
+
+		const staffBuffer = Buffer.from(staffTranscriptText.join('\n'), 'utf-8')
+		const staffTranscriptAttachment = new AttachmentBuilder(staffBuffer, {
+			name: "staff-transcript.txt",
+			description: "A staff-level transcript of the last ModMail conversation."
 		})
 		const embed = new EmbedBuilder()
 			.setTitle("Ticket Closed")
@@ -24,7 +30,7 @@ export default async function ticketCloseFlow(message: Message) {
 			.setColor(0x770202)
 			.setFooter({ text: "At The Mile ModMail" })
 		await (await client.client.users.fetch(user)).send({ files: [ transcriptAttachment ], embeds: [ embed ] })
-		await (await (await client.client.guilds.fetch(settings.GUILD_ID)).channels.fetch(settings.transcript_channel) as TextChannel).send({ files: [ transcriptAttachment ], content: `Transcript from ModMail thread with user ${user} (<@${user}>)` })
+		await (await (await client.client.guilds.fetch(settings.GUILD_ID)).channels.fetch(settings.transcript_channel) as TextChannel).send({ files: [ staffTranscriptAttachment ], content: `Transcript from ModMail thread with user ${user} (<@${user}>)` })
 		await (await (await client.client.guilds.fetch(settings.GUILD_ID)).channels.fetch(channelId))?.delete("Ticket closed.")
 		await ModMailPrisma.DELETE.removeTicket(user)
 		return [true, user, message.author.id]
