aaa90173e6f5ef65eed7b7e9110d94b8281c560a
Tyler <88517473+tm21cy@users.noreply.github.com>
2024-06-14 01:53:46 -0400

    chore: restyle and comment on listeners

--- src/events/incoming.ts
+++ src/events/incoming.ts
@@ -5,12 +5,24 @@ import ticketOpenFlow from "../handlers/ticketOpen";
 import ticketReplyFlow from "../handlers/ticketReply";
 import { Permit } from "../@types/types";
 
+/**
+ * Handles all **incoming** user queries.
+ * An incoming query is defined as a query that comes to the bot via a DM.
+ * Occasionally, this listener is used for hidden staff messages in ticket channels.
+ * @author Tyler
+ * @version 0.1
+ * @since 0.1.0
+ */
 module.exports = {
 	name: Events.MessageCreate,
 	once: false,
 	async execute(message: Message) {
+		
 		if (message.author.bot) return
+
 		switch (message.channel.type) {
+
+			// Handles incoming DM messages.
 			case ChannelType.DM: {
 				const permit = await ModMailPrisma.GET.getUserPermit(message.author.id)
 				if (permit < Permit.EARLY_ACCESS_STAFF) return await message.reply({ content: "You currently do not have permission to access this ModMail feature." })
@@ -24,6 +36,8 @@ module.exports = {
 
 				return LogEmitter.emit('userMessage', message.author.id)
 			}
+
+			// Handles hidden messages.
 			case ChannelType.GuildText: {
 				const status = await ModMailPrisma.GET.getTicketUserByChannel(message.channel.id)
 				if (!status) return
--- src/events/messageEdit.ts
+++ src/events/messageEdit.ts
@@ -3,28 +3,43 @@ import ModMailPrisma from "../api/ModMail";
 import client from "../index";
 import settings from "../settings.json"
 
+/**
+ * Handles all message edit events in a DM - instances where the user wishes to update their message seamlessly.
+ * @author Tyler
+ * @version 0.1
+ * @since 0.1.0
+ */
 module.exports = {
     name: Events.MessageUpdate,
     once: false,
     async execute(oldMessage: Message | PartialMessage, newMessage: Message | PartialMessage) {
+
+        // Resolve partials if they exist
         if (oldMessage.partial) {
             oldMessage = await oldMessage.fetch()
         }
         if (newMessage.partial) {
             newMessage = await newMessage.fetch()
         }
+
         if (oldMessage.channel.type != ChannelType.DM) return
+
         if (!newMessage.editedAt) return
+
         const oldMessageId = oldMessage.id
         const authorId = oldMessage.author.id
         const newContent = newMessage.content
+        
         const modMailStatus = await ModMailPrisma.GET.getUserTicketObject(oldMessage.author.id)
         if (!modMailStatus) return
+        
         const messageEdit = await ModMailPrisma.PATCH.editMessageContent(authorId, newContent, oldMessageId)
         if (!(await ModMailPrisma.GET.isMessageValid(authorId, oldMessageId)) || !messageEdit) return
+        
         const guild = await client.client.guilds.fetch(settings.GUILD_ID)
         const channel = await guild.channels.fetch(modMailStatus.channel!) as TextBasedChannel
         const oldStaffMessage = await channel.messages.fetch(messageEdit.staffMsgId)
+        
         const newEmbed = new EmbedBuilder()
             .setColor(0x770202)
             .setFooter({ text: "Ticket User" })
